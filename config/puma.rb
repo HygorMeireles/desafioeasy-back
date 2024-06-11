7ea59436cdb290629f8e335572f7d9130e06f161
HygorMeireles <hygorgabriel23@hotmail.com>
2024-06-11 16:02:02 -0300

    Deploy2

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 unless ENV.fetch("RAILS_ENV", "production") == "production"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3001 }
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 
