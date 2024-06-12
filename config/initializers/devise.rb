d43eb1d922603ec4e6a4bddab63a1b2af63629d9
HygorMeireles <hygorgabriel23@hotmail.com>
2024-06-12 09:59:08 -0300

    Deploy3

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -41,13 +41,14 @@ Devise.setup do |config|
 
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
+      ['POST', %r{^/user/sign_in$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
+      ['DELETE', %r{^/user/sign_out$}]
     ]
-    jwt.expiration_time = 1.week.to_i
+
+    jwt.expiration_time = 1.year.to_i
 end
 end
