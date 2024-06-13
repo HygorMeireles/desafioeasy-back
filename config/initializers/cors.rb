8451f42535e8ae69b72d782a4799e1efbf4e2315
HygorMeireles <hygorgabriel23@hotmail.com>
2024-06-13 11:11:47 -0300

    Deploy10

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "http://localhost:3002"
+     origins "https://desafioeasy-front.onrender.com"
 
      resource "*",
        headers: :any,
